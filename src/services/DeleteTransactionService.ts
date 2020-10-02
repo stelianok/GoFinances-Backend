4929c34e71668ede53f2be326c0cd40f194ea5b9
Kauã Steliano <stelianok@gmail.com>
2020-10-02 15:36:07 -0300

    Finished Challenge :D

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 // import AppError from '../errors/AppError';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
+  public async execute(id: string): Promise<void> {
     // TODO
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError("Transaction doesn't exist");
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
